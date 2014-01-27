99e51e15218adbb13cc39a766b4a54104eade564
João Netto <hi@joaonetto.me>
2014-01-27 16:32:19 -0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+# Rails Core
+gem 'rails', '4.0.2'
+gem 'sqlite3'
+# gem 'pg'
+gem 'jbuilder', '~> 1.2'
+gem 'unicorn'
+
+# Assets
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+
+# Interface
+gem 'bootstrap-sass', '~> 3.0.3.0'
+
+# Development
+gem 'quiet_assets', :group => :development
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mygoal::Application.config.secret_key_base = 'e7d67ee982c706cc669739e2bc839e5b6f8b589caecd91819cd93444c6dd7f5e80ade55bf0e40bb90c74f4949b024917b29567c783a48010f54bff4e3bef40e9'
